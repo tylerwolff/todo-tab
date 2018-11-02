fa30d7876209006a7cc37ee4b3f70099c8bf9241
tylerwolff <tylerwolff@gmail.com>
2018-11-02 16:29:05 -0400

    add janky todo input

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { format } from "date-fns";
 import Todos from "./Todos";
 import "./App.css";
 
 export default props => {
   const [todos, setTodos] = useState([]);
+  const [time, setTime] = useState(new Date());
+
+  useEffect(
+    () => {
+      document.title = `${todos.length} tasks today`;
+    },
+    [todos]
+  );
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTime(new Date());
+    }, 30000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  });
 
   return (
     <div className="App">
-      <h1 className="todos">{format(new Date(), "dddd HH:mm")}</h1>
-      <Todos todos={todos} onChange={setTodos} />
+      <h1 className="todos">{format(time, "dddd HH:mm")}</h1>
+      <Todos todos={todos} setTodos={setTodos} />
     </div>
   );
 };
--- src/Todos.js
+++ src/Todos.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default props => {
+  const [todo, setTodo] = useState("");
+  const { todos, setTodos } = props;
+
+  const handleChange = e => {
+    setTodo(e.target.value);
+  };
+
   return (
-    <ul>
-      {props.todos.map((t, index) => (
-        <li key={index}>{t.value}</li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {todos.map((t, index) => (
+          <li key={index}>
+            {t.isDone}
+            {t.value}
+          </li>
+        ))}
+      </ul>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          setTodos([...todos, { value: todo, isDone: false }]);
+          setTodo("");
+        }}
+      >
+        <input type="text" value={todo} onChange={handleChange} />
+      </form>
+    </div>
   );
 };
